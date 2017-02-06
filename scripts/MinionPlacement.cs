0492298484852a517c3815417f5643e0ff1d232e
Bobby Tagget <bobby.tagget@gmail.com>
2017-02-06 16:55:43 +1100

    Started programming for cursor guiding camera in minion placement

---
+++ scripts/MinionPlacement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionPlacement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		if(transform.position.y > 0.5 && transform.position.y < 8)
+		transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z);
+		//Debug.Log(transform.position);
+	}
+}
---
+++ scripts/PlacementCamera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacementCamera : MonoBehaviour {
+	public GameObject player;
+	public Material skyGraphic;
+	public float moveSpeed;
+
+	// CAMERA STARTING DISTANCE FROM PLAYER
+	// ====================================
+
+	void Start () {
+		RenderSettings.skybox = skyGraphic;
+	}
+
+	void Update () {
+		Vector3 playerPos = player.transform.position;
+		//	RETAIN SAME DISTANCE FROM PLAYER
+		//	================================
+		Debug.Log(playerPos.x);
+		if(playerPos.x > -7.5f && playerPos.x < 8.5f)
+		{
+			Vector3 newPos = new Vector3(playerPos.x, transform.position.y, transform.position.z);
+			transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * moveSpeed);
+		}
+		// QUIT GAME IN APPLICATION
+		// ========================
+
+		if( Input.GetKeyDown ( KeyCode.Escape ) )
+		{
+			Application.Quit ();
+		}
+	}
+}
