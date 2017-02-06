59d18823a94f35e5f143d24d77ce87f60ca651b2
Bobby Tagget <bobby.tagget@gmail.com>
2017-02-06 19:53:12 +1100

    mouse responsively moves camera. Started coding mouse movement constraints

--- scripts/CameraController.cs
+++ scripts/CameraController.cs
@@ -2,24 +2,43 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-	public GameObject player;
-	private Vector3 distance;
-	public Material skyGraphic;
 
 	// CAMERA STARTING DISTANCE FROM PLAYER
 	// ====================================
 
+	private Vector3 cameraPos;
+	public float moveSpeed;
+	public GameManager gameManager;
+
 	void Start () {
-		distance = player.transform.position - transform.position;
-		RenderSettings.skybox = skyGraphic;
+		cameraPos = transform.position;
 	}
 
 	void Update () {
-		 
-		//	RETAIN SAME DISTANCE FROM PLAYER
-		//	================================
 
-		transform.position = player.transform.position - distance;
+		//store the two edge points of the camera
+		//=======================================
+		float cameraMin = Camera.main.ScreenToWorldPoint(transform.position).x;
+		float cameraMax = Camera.main.ViewportToWorldPoint(transform.position).x;
+
+		Debug.Log(cameraMax);
+		//if(cameraMin <= gameManager.getWorldStart())
+			
+		//Debug.Log(cameraMax);
+
+		//	MOVE CAMERA WITH MOUSE UNTIL WORLD EDGE IS HIT
+		//	==============================================
+
+
+			//if(cameraMin >= gameManager.getWorldStart() && cameraMax <= gameManager.getWorldEnd()){
+
+				//move camera with mouse along the x-axis while keeping y and z axis constraint to starting positions
+				Vector3 mousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, cameraPos.y, cameraPos.z);
+
+				//smooth transition
+				transform.position = Vector3.Lerp(transform.position, cameraPos + (mousePos - cameraPos), Time.deltaTime * moveSpeed);
+
+		    //}
 
 		// QUIT GAME IN APPLICATION
 		// ========================
---
+++ scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public Transform baseOfLevel;
+	float worldStart;
+	float worldEnd;
+
+	// Use this for initialization
+	void Start () {
+		Transform baseOfLevel = GameObject.FindGameObjectWithTag("Level Base").transform;
+		worldStart = baseOfLevel.position.x - getObjectsCentrePoint(baseOfLevel);
+		worldEnd = baseOfLevel.position.x + getObjectsCentrePoint(baseOfLevel);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public float getObjectsCentrePoint (Transform gameObject) {
+
+		//width in unity units / 2
+		return gameObject.localScale.x / 2;
+	}
+
+	public float getWorldStart () { 
+		return worldStart;
+	}
+
+	public float getWorldEnd () {
+		return worldEnd;
+	}
+}
